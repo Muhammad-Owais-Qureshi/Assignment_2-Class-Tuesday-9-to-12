a3eab9bfef00df6825d046bd23653258211cb9c4
Muhammad Owais Qureshi <owaisqureshi34667@gmail.com>
2024-02-28 23:47:04 +0500

    Data Type

---
+++ main.js
@@ -0,0 +1,11 @@
+// Data type in TypeScript
+var FirstName = 'owais qureshi';
+console.log(FirstName, '"string"');
+var Age = 31;
+console.log(Age, '"number"');
+var bool = true;
+console.log(bool);
+var nul = null;
+console.log(nul);
+var und;
+console.log(und);
---
+++ main.ts
@@ -0,0 +1,11 @@
+// Data type in TypeScript
+let FirstName:string = 'owais qureshi';
+console.log(FirstName,'"string"')
+let Age:number = 31;
+console.log(Age ,'"number"')
+let bool:boolean = true;
+console.log(bool)
+let nul : null = null;
+console.log(nul)
+let und : undefined;
+console.log(und)
